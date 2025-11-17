48340f49019640cb74ffd72b1c4c97eb3439dbc8
Vanessa <vcondorie@unsa.edu.pe>
2025-11-17 10:09:34 -0500

    EJ4

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+//EJERCICIO 03
 const primerH2 = document.querySelector("h2");
 console.log("Primer h2:", primerH2.textContent);
+
+//EJERCICIO 04
+document.querySelectorAll("p").forEach(p => {
+    p.textContent = "Texto actualizado dinámicamente";
+});
